4fe426113122518c448125b4fbf448b251201495
Tom Rubin <tomruibin@163.com>
2025-03-11 18:33:18 +0800

    Socket -> Http 线程池方案去除feign

--- src/main/java/com/atom/netpaygateway/client/HttpClientSingle.java
+++ src/main/java/com/atom/netpaygateway/client/HttpClientSingle.java
@@ -1,16 +1,15 @@
 package com.atom.netpaygateway.client;
 
 import com.atom.netpaygateway.enums.EnumRespCode;
-import feign.Feign;
-import feign.Request;
-import feign.Retryer;
-import feign.codec.Decoder;
-import feign.codec.Encoder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.TimeUnit;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Http 连接管理-单笔请求
@@ -54,18 +53,40 @@ public class HttpClientSingle {
      */
     public String sendMessage(String message) {
         log.info("发送Http：{}，连接超时：{}秒，接收超时：{}秒", url, connectTimeout, readTimeout);
-        HttpFeignExtClinet feignExtClinet = Feign.builder()
-                .encoder(new Encoder.Default())
-                .decoder(new Decoder.Default())
-                .options(new Request.Options(connectTimeout, TimeUnit.SECONDS, readTimeout, TimeUnit.SECONDS, true))
-                .retryer(canResend ? new Retryer.Default() : Retryer.NEVER_RETRY)
-                .target(HttpFeignExtClinet.class, url);
 
         try {
-            return feignExtClinet.sendMessage(message);
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type", "application/xml;charset=UTF-8");
+            con.setDoOutput(true);
+            con.setConnectTimeout(connectTimeout * 1000);
+            con.setReadTimeout(readTimeout * 1000);
+
+            try (OutputStream os = con.getOutputStream()) {
+                byte[] input = message.getBytes("utf-8");
+                os.write(input, 0, input.length);
+            }
+
+            int responseCode = con.getResponseCode();
+            log.info("Http 响应码 : {}", responseCode);
+
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine).append("\n");
+                }
+                return response.toString();
+            }
         } catch (Exception e) {
             log.warn("Http 调用异常", e);
+            if (canResend) {
+                log.info("尝试重新发送请求");
+                return sendMessage(message); // 递归重试
+            }
             return EnumRespCode.FAIL.getCode();
         }
     }
 }
+
